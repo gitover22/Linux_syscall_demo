fe0dce7f90490dfb4e04c833b5db11323a28ea4a
gitover22 <qidizou88@gmail.com>
2024-04-30 12:15:00 +0800

    mod pthread_mutexs.c

--- demos/pthread_mutexs.c
+++ demos/pthread_mutexs.c
@@ -3,8 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#define READ_THHREAD_NUM 10
-#define WRITE_THHREAD_NUM 5
+#define THREAD_NUM 5
 /**
  * gdb 调试多线程程序
  * info threads ：展示所有线程
@@ -32,7 +31,7 @@ shared_data data = {
 void *read_function(void *argv){
     pthread_mutex_lock(&mutex);
     sleep(1);
-    printf("thread %ld read_data: %d read_str: %s\n",pthread_self(), data.num,data.str);
+    printf("read thread %ld read data: %d , read str: %s\n",pthread_self(), data.num,data.str);
     pthread_mutex_unlock(&mutex);
 }
 
@@ -40,9 +39,9 @@ void *read_function(void *argv){
 void *write_function(void *argv){
     pthread_mutex_lock(&mutex);
     data.num++;
-    printf("thread %ld write_data: %d\n", pthread_self(),data.num);
     sleep(1);
     char num_str[10];
+    printf("write thread %ld write_data: %d\n", pthread_self(),data.num);
     sprintf(num_str, "%d", data.num); // 将整数转换为字符串
     strcat(data.str, " "); // 先加一个空格
     strcat(data.str, num_str); // 将转换后的整数字符串追加到 data.str 中
@@ -51,22 +50,20 @@ void *write_function(void *argv){
 
 int main(){
     data.str = (char *)malloc(sizeof(char) * 100);
-    strcpy(data.str, "hello:");
-    pthread_t read_thread[READ_THHREAD_NUM];
-    pthread_t write_thread[WRITE_THHREAD_NUM];
+    strcpy(data.str, "hello ");
+    pthread_t read_thread[THREAD_NUM];
+    pthread_t write_thread[THREAD_NUM];
     int i;
-    for(i = 0; i < READ_THHREAD_NUM; i++){
+    for(i = 0; i < THREAD_NUM; i++){
         pthread_create(&read_thread[i], NULL, read_function,NULL);
-    }
-    for(i = 0; i < WRITE_THHREAD_NUM; i++){
         pthread_create(&write_thread[i], NULL, write_function, NULL);
     }
-    for(i = 0; i < READ_THHREAD_NUM; i++){
+    // 等待所有线程结束
+    for(i = 0; i < THREAD_NUM; i++){
         pthread_join(read_thread[i], NULL);
-    }
-    for(i = 0; i < WRITE_THHREAD_NUM; i++){
         pthread_join(write_thread[i], NULL);
     }
+    printf("data.str: %s\n", data.str);
     free(data.str);
     pthread_mutex_destroy(&mutex);
     return 0;
